9a61b3e1597874d4919e4d942b480c7f27d1e437
unknown <73888966+rosavanree@users.noreply.github.com>
2021-04-30 13:13:08 +0200

    kleine aanpassingen

--- Main_test.py
+++ Main_test.py
@@ -791,6 +791,7 @@ class Ui(QtWidgets.QMainWindow):
         self.instellingen_close.setEnabled(False)
         self.instelling_grid.lower()
         self.instelling_widg.lower()
+        #load_page(self, self.page_nr)
 
     def set_small_buttons(self):
         self.huidig_kind.knopgr = 0
@@ -827,7 +828,6 @@ class Ui(QtWidgets.QMainWindow):
         self.letters_groot.setStyleSheet("QPushButton{color:grey;text-decoration:none;border-top:3px transparent;border-bottom: 3px transparent;border-right: 10px transparent;border-left: 10px transparent;}")
         # @Guus er moet zoiets bij, dat de caption insta groter/kleiner wordt en niet dat je de pagina moet herladen, kan jij hier naar kijken
         #set_caption(self.pagenr)
-        self.load_page(self, self.page_nr)
     
     def font_medium(self):
         self.huidig_kind.font_size = 13 
@@ -836,7 +836,7 @@ class Ui(QtWidgets.QMainWindow):
         self.letters_groot.setStyleSheet("QPushButton{color:grey;text-decoration:none;border-top:3px transparent;border-bottom: 3px transparent;border-right: 10px transparent;border-left: 10px transparent;}")
         # @Guus hier ook
         #set_caption(self.pagenr)
-        self.load_page(self, self.page_nr)
+        
     def font_large(self):
         self.huidig_kind.font_size = 15
         self.letters_klein.setStyleSheet("QPushButton{color:grey;text-decoration:none;border-top:3px transparent;border-bottom: 3px transparent;border-right: 10px transparent;border-left: 10px transparent;}")
@@ -844,7 +844,7 @@ class Ui(QtWidgets.QMainWindow):
         self.letters_groot.setStyleSheet("QPushButton{color:orange;text-decoration:underline;border-top:3px transparent;border-bottom: 3px transparent;border-right: 10px transparent;border-left: 10px transparent;}")
         # @Guus hier ook
         #set_caption(self.pagenr)
-        self.load_page(self, self.page_nr)
+       
     def geluid_aan_uit(self):
         self.huidig_kind.geluid_zichtbaar += 1
         if (self.huidig_kind.geluid_zichtbaar%2) == 1:
@@ -882,5 +882,5 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = Ui(app)
     window.show()
-    sys.exit()
-    #sys.exit(app.exec_()) 
+    #sys.exit()
+    sys.exit(app.exec_()) 
